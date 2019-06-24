5ad4cb36e8a8be34427b0fb1add7009c6f5baec0
Benjamin Hall <hallb1016@gmail.com>
2019-06-24 14:40:42 -0400

    Added actual code

---
+++ AdminBot/CommandHandler.cs
@@ -0,0 +1,71 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AdminBot
+{
+    class CommandHandler
+    {
+        public const char prefix = '\\';
+
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _service;
+        private readonly IServiceProvider _services;
+
+        public CommandHandler(DiscordSocketClient client, IServiceProvider services)
+        {
+            _client = client;
+            _services = services;
+
+            _service = new CommandService();
+            _service.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+
+            _client.Connected += SendConnectMessage;
+            _client.Disconnected += SendDisconnectError;
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task SendConnectMessage()
+        {
+            if (Program.isConsole)
+            {
+                await Console.Out.WriteLineAsync($"{SecurityInfo.botName} Online");
+            }
+        }
+
+        private async Task SendDisconnectError(Exception e)
+        {
+            if (Program.isConsole)
+            {
+                await Console.Out.WriteLineAsync(e.Message);
+            }
+        }
+
+        private async Task HandleCommandAsync(SocketMessage m)
+        {
+            if (!(m is SocketUserMessage msg))
+            {
+                return;
+            }
+
+            SocketCommandContext context = new SocketCommandContext(_client, msg);
+
+            int argPos = 0;
+            if (!context.User.IsBot)
+            {
+                // Commands
+                if (msg.HasCharPrefix(prefix, ref argPos))
+                {
+                    var result = await _service.ExecuteAsync(context, argPos, _services);
+
+                    if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    {
+                        await context.Channel.SendMessageAsync($"Error: {result.ErrorReason}");
+                    }
+                }
+            }
+        }
+    }
+}
