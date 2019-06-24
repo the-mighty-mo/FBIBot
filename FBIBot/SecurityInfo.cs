854822b188e5e7b85f7f1df35fa1cffd66dcee54
Benjamin Hall <hallb1016@gmail.com>
2019-06-24 15:41:56 -0400

    Changed bot name

--- FBIBot/SecurityInfo.cs
+++ FBIBot/SecurityInfo.cs
@@ -7,7 +7,7 @@ namespace FBIBot
         public const string token = ""; // Bot token
         public const string botID = ""; // Bot discriminator (four numbers after the #)
         public const ulong botClient = 0; // Client ID
-        public const string botName = "FBIBot";
+        public const string botName = "The FBI";
         public const string botVersion = "1.0.0.0";
         public static readonly Color botColor = new Color(17, 128, 106);
     }
