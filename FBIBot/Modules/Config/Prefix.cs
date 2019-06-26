aaca6d27ee4685b83a85d905a3c2790aafed1e2a
Benjamin Hall <hallb1016@gmail.com>
2019-06-26 12:04:02 -0400

    Cleaned up usings

--- FBIBot/Modules/Config/Prefix.cs
+++ FBIBot/Modules/Config/Prefix.cs
@@ -1,10 +1,6 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FBIBot.Modules.Config
--- FBIBot/Modules/Config/VerifyRole.cs
+++ FBIBot/Modules/Config/VerifyRole.cs
@@ -2,10 +2,6 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FBIBot.Modules.Config
