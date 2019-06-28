ff3a327fc9f4f249dcee30cc1a35d0f0e9082798
Benjamin Hall <hallb1016@gmail.com>
2019-06-27 20:00:04 -0400

    Cleaned up usings

--- FBIBot/Modules/Mod/GetWarnings.cs
+++ FBIBot/Modules/Mod/GetWarnings.cs
@@ -1,10 +1,7 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Data.Sqlite;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FBIBot.Modules.Mod
