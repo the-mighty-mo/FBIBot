f04e92c1d42ad2d273b813c66ef22431a4aae101
Benjamin Hall <hallb1016@gmail.com>
2019-07-08 18:39:42 -0400

    Optimized the hierarchy requires methods

--- FBIBot/Preconditions/RequireBotHierarchy.cs
+++ FBIBot/Preconditions/RequireBotHierarchy.cs
@@ -11,17 +11,16 @@ namespace FBIBot
 
         public RequireBotHierarchy(string cmd = "run this command on") => command = cmd;
 
-        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext Context, ParameterInfo parameter, object value, IServiceProvider services) =>
-            value is SocketGuildUser user
-                ? await CheckPermissionsAsync(Context as SocketCommandContext, user)
-                : value is string userId && ulong.TryParse(userId, out ulong userID)
-                    ? await CheckPermissionsAsync(Context as SocketCommandContext, userID)
-                    : PreconditionResult.FromError("Our intelligence team has informed us that the given user does not exist.");
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, ParameterInfo parameter, object value, IServiceProvider services)
+        {
+            SocketCommandContext Context = context as SocketCommandContext;
 
-        private async Task<PreconditionResult> CheckPermissionsAsync(SocketCommandContext Context, ulong userID) =>
-            Context.Guild.GetUser(userID) != null
-                ? await CheckPermissionsAsync(Context, Context.Guild.GetUser(userID))
-                : PreconditionResult.FromError("Our intelligence team has informed us that the given user does not exist.");
+            return value is SocketGuildUser user
+                ? await CheckPermissionsAsync(Context, user)
+                : value is string userId && ulong.TryParse(userId, out ulong userID) && (user = Context.Guild.GetUser(userID)) != null
+                    ? await CheckPermissionsAsync(Context, user)
+                    : PreconditionResult.FromError("Our intelligence team has informed us that the given user does not exist.");
+        }
 
         private async Task<PreconditionResult> CheckPermissionsAsync(SocketCommandContext Context, SocketGuildUser target) =>
             Context.User is SocketGuildUser
